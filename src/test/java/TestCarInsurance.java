65d7e9abf501ad5874851b8db3ba99fb71929528
User <zlobina_christine@mail.ru>
2021-08-23 11:56:23 +0300

    crwgvrey

--- src/test/java/TestCarInsurance.java
+++ src/test/java/TestCarInsurance.java
@@ -1,22 +1,25 @@
-import org.junit.jupiter.api.*;
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
 
 public class TestCarInsurance {
     @Test
     public void InsurancePositive () {
-        Assertions.assertEquals(210, CarInsurance.CountInsurance(0.5,0, 25));
-        Assertions.assertEquals(210, CarInsurance.CountInsurance(2,1, 25));
-        Assertions.assertEquals(210, CarInsurance.CountInsurance(35,2, 25));
-        Assertions.assertEquals(210, CarInsurance.CountInsurance(35,1, 75));
-        Assertions.assertEquals(210, CarInsurance.CountInsurance(2,0, 75));
-        Assertions.assertEquals(210, CarInsurance.CountInsurance(35,0, 125));
-        Assertions.assertEquals(210, CarInsurance.CountInsurance(35,0, 350));
-        Assertions.assertEquals(1200, CarInsurance.CountInsurance(0.5,1000, 75));
-        Assertions.assertEquals(1200, CarInsurance.CountInsurance(0.5,2, 350));
-        Assertions.assertEquals(1200, CarInsurance.CountInsurance(0.5,2, 350));
-        Assertions.assertEquals(1050, CarInsurance.CountInsurance(0.5,2, 125));
-        Assertions.assertEquals(1680, CarInsurance.CountInsurance(0.5,1000, 125));
-        Assertions.assertEquals(251, CarInsurance.CountInsurance(2,0, 125));
-        Assertions.assertEquals(1280, CarInsurance.CountInsurance(2,1000, 350));
+        assertEquals(210, CarInsurance.CountInsurance(0.5,0, 25));
+        assertEquals(210, CarInsurance.CountInsurance(2,1, 25));
+        assertEquals(210, CarInsurance.CountInsurance(35,2, 25));
+        assertEquals(210, CarInsurance.CountInsurance(35,1, 75));
+        assertEquals(210, CarInsurance.CountInsurance(2,0, 75));
+        assertEquals(210, CarInsurance.CountInsurance(35,0, 125));
+        assertEquals(210, CarInsurance.CountInsurance(35,0, 350));
+        assertEquals(1200, CarInsurance.CountInsurance(0.5,1000, 75));
+        assertEquals(1200, CarInsurance.CountInsurance(0.5,2, 350));
+        assertEquals(1200, CarInsurance.CountInsurance(0.5,2, 350));
+        assertEquals(1050, CarInsurance.CountInsurance(0.5,2, 125));
+        assertEquals(1680, CarInsurance.CountInsurance(0.5,1000, 125));
+        assertEquals(251, CarInsurance.CountInsurance(2,0, 125));
+        assertEquals(1280, CarInsurance.CountInsurance(2,1000, 350));
 
     }
   }
